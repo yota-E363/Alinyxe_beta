b6620612108cb51ab36808b3dbe8bdf8a5a9b7f8
yota-E363 <bouboulejean622@gmail.com>
2025-12-26 08:47:44 +0100

    origin

---
+++ file_structure.fs
@@ -0,0 +1,247 @@
+├── □ Alinyxe_beta/-
+│   ├── □ public/-
+│   │   ├── favicon.ico
+│   │   ├── placeholder.svg
+│   │   └── robots.txt
+│   ├── □ src/-
+│   │   ├── □ assets/-
+│   │   │   ├── 1.jpg
+│   │   │   ├── 2.jpg
+│   │   │   ├── 3.jpg
+│   │   │   ├── 4.jpg
+│   │   │   ├── hero-bg.jpg
+│   │   │   └── logo.png
+│   │   ├── □ components/-
+│   │   │   ├── □ ui/-
+│   │   │   │   ├── accordion.tsx
+│   │   │   │   ├── alert-dialog.tsx
+│   │   │   │   ├── alert.tsx
+│   │   │   │   ├── aspect-ratio.tsx
+│   │   │   │   ├── avatar.tsx
+│   │   │   │   ├── badge.tsx
+│   │   │   │   ├── breadcrumb.tsx
+│   │   │   │   ├── button.tsx
+│   │   │   │   ├── calendar.tsx
+│   │   │   │   ├── card.tsx
+│   │   │   │   ├── carousel.tsx
+│   │   │   │   ├── chart.tsx
+│   │   │   │   ├── checkbox.tsx
+│   │   │   │   ├── collapsible.tsx
+│   │   │   │   ├── command.tsx
+│   │   │   │   ├── context-menu.tsx
+│   │   │   │   ├── dialog.tsx
+│   │   │   │   ├── drawer.tsx
+│   │   │   │   ├── dropdown-menu.tsx
+│   │   │   │   ├── form.tsx
+│   │   │   │   ├── hover-card.tsx
+│   │   │   │   ├── input-otp.tsx
+│   │   │   │   ├── input.tsx
+│   │   │   │   ├── label.tsx
+│   │   │   │   ├── menubar.tsx
+│   │   │   │   ├── navigation-menu.tsx
+│   │   │   │   ├── pagination.tsx
+│   │   │   │   ├── popover.tsx
+│   │   │   │   ├── progress.tsx
+│   │   │   │   ├── radio-group.tsx
+│   │   │   │   ├── resizable.tsx
+│   │   │   │   ├── scroll-area.tsx
+│   │   │   │   ├── select.tsx
+│   │   │   │   ├── separator.tsx
+│   │   │   │   ├── sheet.tsx
+│   │   │   │   ├── sidebar.tsx
+│   │   │   │   ├── skeleton.tsx
+│   │   │   │   ├── slider.tsx
+│   │   │   │   ├── sonner.tsx
+│   │   │   │   ├── switch.tsx
+│   │   │   │   ├── table.tsx
+│   │   │   │   ├── tabs.tsx
+│   │   │   │   ├── textarea.tsx
+│   │   │   │   ├── toast.tsx
+│   │   │   │   ├── toaster.tsx
+│   │   │   │   ├── toggle-group.tsx
+│   │   │   │   ├── toggle.tsx
+│   │   │   │   ├── tooltip.tsx
+│   │   │   │   └── use-toast.ts
+│   │   │   ├── About.tsx
+│   │   │   ├── CTA.tsx
+│   │   │   ├── DiversServices.tsx
+│   │   │   ├── Footer.tsx
+│   │   │   ├── HeroSpline.tsx
+│   │   │   ├── Navbar.tsx
+│   │   │   ├── NavLink.tsx
+│   │   │   ├── Partners.tsx
+│   │   │   ├── Pricing.tsx
+│   │   │   ├── Projects.tsx
+│   │   │   └── Testimonial.tsx
+│   │   ├── □ hooks/-
+│   │   │   ├── use-mobile.tsx
+│   │   │   └── use-toast.ts
+│   │   ├── □ lib/-
+│   │   │   └── utils.ts
+│   │   ├── □ pages/-
+│   │   │   ├── Accueil.tsx
+│   │   │   ├── Contact.tsx
+│   │   │   ├── NotFound.tsx
+│   │   │   ├── Pricing.tsx
+│   │   │   └── Services.tsx
+│   │   ├── □ types/-
+│   │   │   └── aos.d.ts
+│   │   ├── App.css
+│   │   ├── App.tsx
+│   │   ├── index.css
+│   │   ├── main.tsx
+│   │   └── vite-env.d.ts
+│   ├── .gitignore
+│   ├── bun.lockb
+│   ├── components.json
+│   ├── eslint.config.js
+│   ├── file_structure.fs
+│   ├── index.html
+│   ├── package.json
+│   ├── postcss.config.js
+│   ├── tailwind.config.js
+│   ├── tailwind.config.ts
+│   ├── tsconfig.app.json
+│   ├── tsconfig.json
+│   ├── tsconfig.node.json
+│   └── vite.config.ts
+└── □ alinyxe-nexus/-
+    ├── □ public/-
+    │   ├── □ images/-
+    │   │   ├── banner.jpg
+    │   │   ├── logo.png
+    │   │   └── sub_logo.jpg
+    │   ├── _headers
+    │   ├── _redirects
+    │   ├── android-chrome-192x192.png
+    │   ├── android-chrome-512x512.png
+    │   ├── apple-touch-icon.png
+    │   ├── favicon-16x16.png
+    │   ├── favicon-32x32.png
+    │   ├── favicon.ico
+    │   ├── placeholder.svg
+    │   ├── robots.txt
+    │   └── sitemap.xml
+    ├── □ src/-
+    │   ├── □ assets/-
+    │   │   ├── □ logos/-
+    │   │   │   ├── amazon.png
+    │   │   │   ├── css.png
+    │   │   │   ├── google.png
+    │   │   │   ├── html.png
+    │   │   │   ├── meta.webp
+    │   │   │   ├── node-js.png
+    │   │   │   ├── prisma.webp
+    │   │   │   ├── react.png
+    │   │   │   └── tailwind.png
+    │   │   ├── 1.jpg
+    │   │   ├── 2.jpg
+    │   │   ├── 3.jpg
+    │   │   ├── 4.jpg
+    │   │   ├── hero-bg.jpg
+    │   │   ├── img.jpg
+    │   │   └── logo.png
+    │   ├── □ components/-
+    │   │   ├── □ ui/-
+    │   │   │   ├── accordion.tsx
+    │   │   │   ├── alert-dialog.tsx
+    │   │   │   ├── alert.tsx
+    │   │   │   ├── animated-gradient.tsx
+    │   │   │   ├── animated-list.tsx
+    │   │   │   ├── aspect-ratio.tsx
+    │   │   │   ├── avatar.tsx
+    │   │   │   ├── badge.tsx
+    │   │   │   ├── border-beam.tsx
+    │   │   │   ├── breadcrumb.tsx
+    │   │   │   ├── button.tsx
+    │   │   │   ├── calendar.tsx
+    │   │   │   ├── card.tsx
+    │   │   │   ├── carousel.tsx
+    │   │   │   ├── chart.tsx
+    │   │   │   ├── checkbox.tsx
+    │   │   │   ├── collapsible.tsx
+    │   │   │   ├── command.tsx
+    │   │   │   ├── context-menu.tsx
+    │   │   │   ├── dialog.tsx
+    │   │   │   ├── drawer.tsx
+    │   │   │   ├── dropdown-menu.tsx
+    │   │   │   ├── form.tsx
+    │   │   │   ├── hover-card.tsx
+    │   │   │   ├── input-otp.tsx
+    │   │   │   ├── input.tsx
+    │   │   │   ├── label.tsx
+    │   │   │   ├── menubar.tsx
+    │   │   │   ├── navigation-menu.tsx
+    │   │   │   ├── pagination.tsx
+    │   │   │   ├── popover.tsx
+    │   │   │   ├── progress.tsx
+    │   │   │   ├── radio-group.tsx
+    │   │   │   ├── resizable.tsx
+    │   │   │   ├── scroll-area.tsx
+    │   │   │   ├── select.tsx
+    │   │   │   ├── separator.tsx
+    │   │   │   ├── sheet.tsx
+    │   │   │   ├── sidebar.tsx
+    │   │   │   ├── skeleton.tsx
+    │   │   │   ├── slider.tsx
+    │   │   │   ├── sonner.tsx
+    │   │   │   ├── switch.tsx
+    │   │   │   ├── table.tsx
+    │   │   │   ├── tabs.tsx
+    │   │   │   ├── textarea.tsx
+    │   │   │   ├── toast.tsx
+    │   │   │   ├── toaster.tsx
+    │   │   │   ├── toggle-group.tsx
+    │   │   │   ├── toggle.tsx
+    │   │   │   ├── tooltip.tsx
+    │   │   │   └── use-toast.ts
+    │   │   ├── About.tsx
+    │   │   ├── CTA.tsx
+    │   │   ├── DiversService.tsx
+    │   │   ├── DiversServices.tsx
+    │   │   ├── Footer.tsx
+    │   │   ├── HeroSpline.tsx
+    │   │   ├── Navbar.tsx
+    │   │   ├── NavLink.tsx
+    │   │   ├── Parteners.tsx
+    │   │   ├── Partners.tsx
+    │   │   ├── Pricing.tsx
+    │   │   ├── Projects.tsx
+    │   │   ├── ScrollToTop.tsx
+    │   │   └── Testimonial.tsx
+    │   ├── □ hooks/-
+    │   │   ├── use-mobile.tsx
+    │   │   └── use-toast.ts
+    │   ├── □ lib/-
+    │   │   └── utils.ts
+    │   ├── □ pages/-
+    │   │   ├── Accueil.tsx
+    │   │   ├── Contact.tsx
+    │   │   ├── NotFound.tsx
+    │   │   ├── Pricing.tsx
+    │   │   └── Services.tsx
+    │   ├── □ types/-
+    │   │   └── aos.d.ts
+    │   ├── □ utils/-
+    │   │   └── analytics.js
+    │   ├── App.css
+    │   ├── App.tsx
+    │   ├── index.css
+    │   ├── main.tsx
+    │   └── vite-env.d.ts
+    ├── .gitignore
+    ├── bun.lockb
+    ├── components.json
+    ├── eslint.config.js
+    ├── generate-sitemap.mjs
+    ├── index.html
+    ├── package-lock.json
+    ├── package.json
+    ├── postcss.config.js
+    ├── README.md
+    ├── tailwind.config.js
+    ├── tailwind.config.ts
+    ├── tsconfig.app.json
+    ├── tsconfig.json
+    ├── tsconfig.node.json
+    └── vite.config.ts
